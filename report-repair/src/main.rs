0d124c5445aca2ec8c963d1229d225f7c48d0a17
Viktor Charypar <charypar@gmail.com>
2020-12-18 15:42:13 +0000

    Day 1 part 2

--- report-repair/src/main.rs
+++ report-repair/src/main.rs
@@ -1,31 +1,84 @@
+use std::collections::HashMap;
 use std::io::{self, BufRead};
 
+struct Index {
+    numbers: Vec<usize>,
+    pairwise_sums: HashMap<usize, (usize, usize)>,
+}
+
+impl Index {
+    fn new() -> Index {
+        Index {
+            numbers: Vec::new(),
+            pairwise_sums: HashMap::new(),
+        }
+    }
+
+    fn check_pair(&self, n: usize, total: usize) -> Option<usize> {
+        for num in &self.numbers {
+            if n + num == total {
+                return Some(*num);
+            }
+        }
+
+        None
+    }
+
+    fn check_triple(&self, n: usize, total: usize) -> Option<(usize, usize)> {
+        for num in self.pairwise_sums.keys() {
+            if n + num == total {
+                return Some(self.pairwise_sums[num]);
+            }
+        }
+
+        None
+    }
+
+    fn push(&mut self, n: usize) {
+        for num in &self.numbers {
+            self.pairwise_sums.insert(*num + n, (*num, n));
+        }
+        self.numbers.push(n);
+    }
+
+    #[allow(dead_code)]
+    fn push_vec(&mut self, ns: Vec<usize>) {
+        for n in ns {
+            self.push(n);
+        }
+    }
+}
+
 fn main() {
-    let mut numbers: Vec<usize> = Vec::new();
+    let mut index = Index::new();
     let total = 2020;
 
     for line in io::stdin().lock().lines() {
         if let Ok(line) = line {
             if let Ok(number) = line.parse::<usize>() {
-                if let Some(pair) = check(&numbers, number, total) {
-                    println!("Found match: {} + {} == {}", number, pair, total,);
-                    println!("Answer is {} * {} = {}", number, pair, number * pair)
-                } else {
-                    numbers.push(number);
+                if let Some(pair) = index.check_pair(number, total) {
+                    println!("Found a pair match: {} + {} == {}", number, pair, total,);
+                    println!("Pair answer is {} * {} = {}", number, pair, number * pair)
                 }
+
+                if let Some(triple) = index.check_triple(number, total) {
+                    println!(
+                        "Found a triple match: {} + {} + {} == {}",
+                        number, triple.0, triple.1, total,
+                    );
+                    println!(
+                        "Triple answer is {} * {} * {} = {}",
+                        number,
+                        triple.0,
+                        triple.1,
+                        number * triple.0 * triple.1
+                    )
+                }
+
+                index.push(number);
             }
         }
     }
-}
-
-fn check(numbers: &Vec<usize>, n: usize, total: usize) -> Option<usize> {
-    for num in numbers {
-        if n + num == total {
-            return Some(*num);
-        }
-    }
-
-    None
 }
 
 #[cfg(test)]
@@ -34,27 +87,42 @@ mod tests {
 
     #[test]
     fn check_empty_list() {
-        let list = Vec::new();
+        let index = Index::new();
         let expected = None;
-        let actual = check(&list, 3, 10);
+        let actual = index.check_pair(3, 10);
 
         assert_eq!(expected, actual);
     }
 
     #[test]
-    fn check_no_match() {
-        let list = vec![1, 2, 3];
+    fn check_no_pair_match() {
+        let mut index = Index::new();
+        index.push_vec(vec![1, 2, 3]);
+
         let expected = None;
-        let actual = check(&list, 3, 10);
+        let actual = index.check_pair(3, 10);
 
         assert_eq!(expected, actual);
     }
 
     #[test]
-    fn check_match() {
-        let list = vec![1, 2, 3, 4, 5, 6, 7, 8];
+    fn check_pair_match() {
+        let mut index = Index::new();
+        index.push_vec(vec![1, 2, 3, 4, 5, 6, 7, 8]);
+
         let expected = Some(7);
-        let actual = check(&list, 3, 10);
+        let actual = index.check_pair(3, 10);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn check_triple_match() {
+        let mut index = Index::new();
+        index.push_vec(vec![1, 2, 3, 4]);
+
+        let expected = Some((3, 4));
+        let actual = index.check_triple(3, 10);
 
         assert_eq!(expected, actual);
     }
