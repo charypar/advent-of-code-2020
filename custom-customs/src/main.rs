06caa06cef849675b3e72584908c4841bd9a7d10
Viktor Charypar <charypar@gmail.com>
2020-12-19 22:58:10 +0000

    Day 6 part 2

--- custom-customs/src/main.rs
+++ custom-customs/src/main.rs
@@ -1,21 +1,28 @@
-use std::{collections::HashSet, io::BufRead};
+use std::{collections::HashMap, io::BufRead};
 
 fn main() {
-    let mut group: HashSet<char> = HashSet::new();
+    let mut group: HashMap<char, usize> = HashMap::new();
+    let mut group_length = 0;
     let mut total = 0;
 
     while let Some(Ok(line)) = std::io::stdin().lock().lines().next() {
         if line == "" {
-            total += group.len();
+            total += group.iter().filter(|(_, v)| **v == group_length).count();
+            group_length = 0;
             group.clear();
+            continue;
         }
 
+        group_length += 1;
         for c in line.chars() {
-            group.insert(c);
+            group.entry(c).and_modify(|n| *n += 1).or_insert(1);
         }
     }
 
-    total += group.len();
+    total += group.iter().filter(|(_, v)| **v == group_length).count();
 
-    println!("Total questions answered: {}", total);
+    println!(
+        "Total questions answered by everyone in the group: {}",
+        total
+    );
 }
