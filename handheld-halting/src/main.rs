b366a2c707e89b906bf5f10c401647b6271ea4a2
Viktor Charypar <charypar@gmail.com>
2020-12-21 10:30:57 +0000

    Day 8 part 2

--- handheld-halting/src/main.rs
+++ handheld-halting/src/main.rs
@@ -10,16 +10,36 @@ fn main() {
         })
         .collect::<Vec<_>>();
 
+    for i in 0..program.len() {
+        let new_op = match program[i] {
+            Op::Nop(n) => Op::Jmp(n),
+            Op::Jmp(n) => Op::Nop(n),
+            Op::Acc(_) => continue,
+        };
+
+        let mut modified_program = program.clone();
+        modified_program[i] = new_op;
+
+        let (pc, acc) = run(&modified_program);
+
+        if pc == program.len() {
+            println!("Program {} ends with pc = {}, acc = {}", i, pc, acc);
+            break;
+        }
+    }
+}
+
+fn run(program: &[Op]) -> (usize, i64) {
     let mut visited = vec![false; program.len()];
 
     let mut acc: i64 = 0;
     let mut pc: usize = 0;
 
-    while !visited[pc] {
+    while pc < program.len() && !visited[pc] {
         visited[pc] = true;
 
         match program[pc] {
-            Op::Nop => pc += 1,
+            Op::Nop(_) => pc += 1,
             Op::Jmp(n) => pc = (pc as i64 + n) as usize,
             Op::Acc(n) => {
                 acc += n;
@@ -28,14 +48,14 @@ fn main() {
         }
     }
 
-    println!("Stopped at pc: {}, acc: {} ", pc, acc);
+    (pc, acc)
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum Op {
     Acc(i64),
     Jmp(i64),
-    Nop,
+    Nop(i64),
 }
 
 fn parse_line(line: &str) -> Op {
@@ -44,7 +64,7 @@ fn parse_line(line: &str) -> Op {
     match parts[0..2] {
         ["acc", num] => Op::Acc(num.parse().unwrap()),
         ["jmp", num] => Op::Jmp(num.parse().unwrap()),
-        ["nop", _] => Op::Nop,
+        ["nop", num] => Op::Nop(num.parse().unwrap()),
         _ => unreachable!(),
     }
 }
