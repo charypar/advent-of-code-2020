e29a5a24729c825c7dd235efd1673bae651a3e17
Viktor Charypar <charypar@gmail.com>
2021-01-01 22:58:01 +0000

    Day 16, part 2

--- ticket-translation/src/main.rs
+++ ticket-translation/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::io::BufRead;
 use std::ops::RangeInclusive;
 use std::str::FromStr;
@@ -27,40 +29,85 @@ fn main() {
         }
     }
 
-    println!("Rules: {:?}", rules);
-    println!("My ticket: {:?}", ticket);
-    println!("Nearby tickets: {:?}", tickets);
+    tickets = tickets
+        .into_iter()
+        .filter(|ticket| ticket.valid(&rules))
+        .collect();
 
-    let invalids = invalids(&tickets, &rules);
-    println!(
-        "Invalid values: {:?}, total = {}",
-        invalids,
-        invalids.iter().sum::<usize>()
-    );
+    let fields = infer_fields(&tickets, &rules);
+
+    let answer: usize = fields
+        .iter()
+        .enumerate()
+        .filter_map(|(i, name)| {
+            if name.starts_with("departure") {
+                Some(ticket.0[i])
+            } else {
+                None
+            }
+        })
+        .product();
+
+    println!("{}", answer);
 }
 
-fn invalids(tickets: &[Ticket], rules: &[Rule]) -> Vec<usize> {
-    let mut invalids = vec![];
+fn infer_fields(tickets: &[Ticket], rules: &[Rule]) -> Vec<String> {
+    let Ticket(first_ticket) = &tickets[0];
 
-    for ticket in tickets {
-        for number in &ticket.0 {
-            let none_match = rules.iter().all(|rule| {
-                let Rule((_, (first, second))) = rule;
+    let mut candidates: Vec<_> = (0..first_ticket.len())
+        .map(|i| {
+            let unique: HashSet<usize> = tickets.iter().map(|ticket| ticket.0[i]).collect();
 
-                !first.contains(&number) && !second.contains(&number)
-            });
+            let matching = rules
+                .iter()
+                .filter_map(|rule| {
+                    let Rule((name, _)) = rule;
 
-            if none_match {
-                invalids.push(*number);
-            }
-        }
-    }
+                    if unique.iter().all(|v| rule.matches(v)) {
+                        Some(name.clone())
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<_>>();
 
-    invalids
+            (i, matching)
+        })
+        .collect();
+
+    candidates.sort_by_key(|(_, cs)| cs.len());
+
+    let mut assigned_fields: HashSet<String> = HashSet::new();
+    let mut fields: Vec<_> = candidates
+        .iter()
+        .map(|(i, cdts)| {
+            let unused: Vec<_> = cdts
+                .iter()
+                .filter(|c| !assigned_fields.contains(*c))
+                .collect();
+
+            assert_eq!(unused.len(), 1);
+            assigned_fields.insert(unused[0].clone());
+
+            (i, unused[0].clone())
+        })
+        .collect();
+
+    fields.sort_by_key(|(i, _)| **i);
+
+    fields.into_iter().map(|(_, f)| f).collect()
 }
 
 #[derive(Debug, PartialEq)]
 struct Rule((String, (RangeInclusive<usize>, RangeInclusive<usize>)));
+
+impl Rule {
+    fn matches(&self, number: &usize) -> bool {
+        let Rule((_, (first, second))) = self;
+
+        first.contains(number) || second.contains(number)
+    }
+}
 
 impl FromStr for Rule {
     type Err = String;
@@ -107,6 +154,15 @@ impl FromStr for Ticket {
     }
 }
 
+impl Ticket {
+    fn valid(&self, rules: &[Rule]) -> bool {
+        !self
+            .0
+            .iter()
+            .any(|number| rules.iter().all(|rule| !rule.matches(number)))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -125,5 +181,27 @@ mod tests {
         let expected = Ticket(vec![7, 1, 14]);
 
         assert_eq!(input.parse::<Ticket>().unwrap(), expected)
+    }
+
+    #[test]
+    fn infers_fields() {
+        let rules: Vec<_> = vec![
+            "class: 0-1 or 4-19",
+            "row: 0-5 or 8-19",
+            "seat: 0-13 or 16-19",
+        ]
+        .iter()
+        .map(|it| it.parse::<Rule>().unwrap())
+        .collect();
+
+        let tickets = vec![
+            Ticket(vec![3, 9, 18]),
+            Ticket(vec![15, 1, 5]),
+            Ticket(vec![5, 14, 9]),
+        ];
+
+        let expected = vec!["row".to_string(), "class".to_string(), "seat".to_string()];
+
+        assert_eq!(infer_fields(&tickets, &rules), expected);
     }
 }
